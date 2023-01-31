f807d76a477e22e39f6979e21525084354aa29aa
hgw <1092165768@qq.com>
2023-01-31 18:53:49 +0800

    first commit

---
+++ src/main/java/com/example/gittest/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.gittest.test;
+
+/**
+ * @Author huangguanwei
+ * @Date
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
